8374df414438cf5260579d5074378fcd13fb979e
kerguo <kergp@live.cn>
2025-08-08 12:59:12 +0800

    test case script

---
+++ test/MyToken.js
@@ -0,0 +1,11 @@
+const {ethers} = require("hardhat")
+
+describe("MyToken", async ()=>{
+    it("test1", async ()=>{
+       console.log("test1 .....")
+    })
+
+    it("test2", async ()=>{
+        console.log("test2 .....")
+     })
+})
