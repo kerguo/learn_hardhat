045f4b883c34868e210d3e657742b6ca6fd137a4
kerguo <kergp@live.cn>
2025-08-08 12:30:25 +0800

    create mytoken.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks:{
+    sepolia:{
+      url:`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  }
 };
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+//0x2AAC7437aA0BB2dd6eBA9bee67F577d47332AECe
